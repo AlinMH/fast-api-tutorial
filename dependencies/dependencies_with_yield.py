92de6711ac388556600a8220a0e6d240b2dc5122
Alin <alin_florin.mihaila@stud.acs.upb.ro>
2020-10-18 16:58:20 +0300

    dependencies done

---
+++ dependencies/dependencies_with_yield.py
@@ -0,0 +1,19 @@
+class DBSession:
+    def close(self):
+        pass
+
+
+class MySuperContextManager:
+    def __init__(self):
+        self.db = DBSession()
+
+    def __enter__(self):
+        return self.db
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db.close()
+
+
+async def get_db():
+    with MySuperContextManager() as db:
+        yield db
